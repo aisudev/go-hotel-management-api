6a188344d9f0023ffe0d59e949adb30b0c2db17c
aisudev <tk.thanatorn@gmail.com>
2021-07-04 01:16:10 +0700

    update crud repo in userRepo

--- feature/user/repository/repo.go
+++ feature/user/repository/repo.go
@@ -17,17 +17,35 @@ func NewUserRepository(db *gorm.DB) domain.UserRepository {
 }
 
 func (r *userRepository) CreateUser(user *domain.User) error {
-	return nil
+	return r.db.Create(&user).Error
 }
 
 func (r *userRepository) GetUser(uuid string) (*domain.User, error) {
-	return nil, nil
+	var user domain.User
+
+	if err := r.db.Where("uuid = ?", uuid).Find(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (r *userRepository) UpdateUser(uuid string, newUser map[string]interface{}) error {
+	var user domain.User
+
+	if err := r.db.Model(&user).Where("uuid = ?", uuid).Updates(newUser).Error; err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (r *userRepository) DeleteUser(uuid string) error {
+	var user domain.User
+
+	if err := r.db.Unscoped().Where("uuid = ?", uuid).Delete(&user).Error; err != nil {
+		return err
+	}
+
 	return nil
 }
